ff0576ce35d9da37bc85ec6c4f10bfb4928bce9a
Brendan Asselstine <mail.asselstine@gmail.com>
2019-01-16 16:49:03 -0800

    Tweaked getreadweb3

--- src/web3/getReadWeb3.js
+++ src/web3/getReadWeb3.js
@@ -6,18 +6,22 @@ import Web3 from 'web3'
 
 export function getReadWeb3(networkId) {
   var providerUrl
-  switch (networkId) {
-    case 1:
-      providerUrl = process.env.REACT_APP_MAINNET_PROVIDER_URL
-      break
-    case 3:
-      providerUrl = process.env.REACT_APP_ROPSTEN_PROVIDER_URL
-      break
-    case 4:
-      providerUrl = process.env.REACT_APP_RINKEBY_PROVIDER_URL
-      break
-    default:
-      providerUrl = process.env.REACT_APP_DEFAULT_PROVIDER_URL
+  if (networkId) {
+    switch (networkId) {
+      case 1:
+        providerUrl = process.env.REACT_APP_MAINNET_PROVIDER_URL
+        break
+      case 3:
+        providerUrl = process.env.REACT_APP_ROPSTEN_PROVIDER_URL
+        break
+      case 4:
+        providerUrl = process.env.REACT_APP_RINKEBY_PROVIDER_URL
+        break
+      default:
+        providerUrl = process.env.REACT_APP_DEFAULT_PROVIDER_URL
+    }
+  } else {
+    providerUrl = process.env.REACT_APP_DEFAULT_PROVIDER_URL
   }
   return new Web3(new Web3.providers.HttpProvider(providerUrl))
 }
