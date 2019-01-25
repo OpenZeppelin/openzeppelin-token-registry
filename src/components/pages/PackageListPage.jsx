3d6ab4e80a26d0d706b719231fcfbc6f7c86ea65
Charles Bergeron <chuck.bergeron@gmail.com>
2019-01-25 14:56:01 -0800

    * convert the homepage to the new hero design & copy

--- src/components/pages/PackageListPage.jsx
+++ src/components/pages/PackageListPage.jsx
@@ -6,13 +6,16 @@ import { Query } from 'react-apollo'
 import { allowedNetworkIds } from '~/web3/allowedNetworkIds'
 import { ScrollToTop } from '~/components/ScrollToTop'
 import { PackageList } from '~/components/packages/PackageList'
+import { PhaseOneHero } from '~/components/packages/PhaseOneHero'
+// import { PreviousHero } from '~/components/packages/PreviousHero'
 import { ResearchersList } from '~/components/researchers/ResearchersList'
 import { web3Queries } from '~/queries/web3Queries'
 import * as routes from '~/../config/routes'
 
 export class PackageListPage extends PureComponent {
   render () {
-    const heroColor = 'is-light'
+    const heroColor = 'is-link'
+    // const heroColor = 'is-light'
     // const heroColor = 'is-dark'
     const showResearchersList = this.props.location.pathname === '/researchers-list'
 
@@ -20,31 +23,9 @@ export class PackageListPage extends PureComponent {
       <div className='is-positioned-absolutely is-full-width'>
         <ScrollToTop />
 
-        <section className={`hero ${heroColor}`}>
-          <div className='hero-body'>
-            <div className='container'>
-              <div className='columns'>
-                <div className='column is-full-desktop is-8-widescreen is-offset-2-widescreen is-6-fullhd is-offset-3-fullhd'>
-                  <div className='has-text-centered'>
-                    <h2 className='title hero--title is-size-1'>
-                      {/* zeppelin<span className='has-text-primary'>OS</span> Beta */}
-                      zeppelinOS Beta
-                    </h2>
-                    <p className='is-size-6'>
-                      Interested in developing your own EVM package?
-                      <br />
-                      Want to vouch for your favourite libraries?
-                    </p>
-                    <br />
-                    <p>
-                      <button className={`button is-pill ${heroColor === 'is-light' ? 'is-black' : 'is-primary'}`}>Sign me up!</button>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        {/* <PreviousHero heroColor={heroColor} /> */}
+        <PhaseOneHero heroColor={heroColor} />
+
         {
           yn(process.env.REACT_APP_NEXT_RELEASE_FEATURE_FLAG) && (
             <section className='section has-level'>
