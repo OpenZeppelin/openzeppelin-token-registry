ca82e3cb3ec1c8b43cf48dedb2763cb4791d82e7
Charles Bergeron <chuck.bergeron@gmail.com>
2019-01-31 14:13:47 -0800

    * cleanup

--- src/components/packages/PhaseOneHero.jsx
+++ src/components/packages/PhaseOneHero.jsx
@@ -8,11 +8,6 @@ export const PhaseOneHero = ({ heroColor }) => {
         <div className='container'>
           <div className='columns'>
             <div className='column is-5-tablet is-offset-1-tablet is-5-desktop is-offset-1-desktop is-4-widescreen is-offset-2-widescreen is-4-fullhd is-offset-2-fullhd'>
-              {/*
-              <h4 className='title hero--title is-size-4 is-hidden-touch'>
-                ZeppelinOS Registry
-              </h4>
-              */}
               <p className='is-size-5'>
                 Introducing EVM packages: the most trusted on-chain libraries.
               </p>
@@ -21,10 +16,6 @@ export const PhaseOneHero = ({ heroColor }) => {
               </p>
             </div>
             <div className='column is-5-tablet is-4-desktop is-offset-1-desktop is-3-widescreen is-3-fullhd is-offset-1-fullhd'>
-              {/* <h5 className='title hero--title is-size-5'>
-                How to use:
-              </h5>
-              */}
               <CodeBox />
               <p>
                 <a href='https://docs.zeppelinos.org' target='_blank' rel='noopener noreferrer'>See EVM Package Docs &gt;</a>
