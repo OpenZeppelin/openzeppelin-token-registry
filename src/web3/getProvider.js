2fb5a513a167895c39ad3d9db3329cb671ef56f6
Charles Bergeron <chuck.bergeron@gmail.com>
2019-02-06 09:47:53 -0800

    * fix bug in various web3 browsers by swapping provider use from window.ethereum to window.web3.currentProvider

--- src/web3/getProvider.js
+++ src/web3/getProvider.js
@@ -1,8 +1,8 @@
 import { ethers } from 'ethers'
 
 export function getProvider () {
-  if (window.ethereum) {
-    return new ethers.providers.Web3Provider(window.ethereum)
+  if (window.web3) {
+    return new ethers.providers.Web3Provider(window.web3.currentProvider)
   } else {
     if (!process.env.REACT_APP_DEFAULT_NETWORK_NAME) {
       throw new Error('You must define the enviroment variable REACT_APP_DEFAULT_NETWORK_NAME')
