9c0e9c4ca68a7653acf176e2140afc6411bf6ab1
Charles Bergeron <chuck.bergeron@gmail.com>
2019-02-06 16:07:38 -0800

    * fix logo placement on obile * improve look of endorsement CTA box * fix bug where buttons were touching on mobile

--- src/components/packages/PackageDetails.jsx
+++ src/components/packages/PackageDetails.jsx
@@ -97,7 +97,6 @@ export class PackageDetails extends Component {
           </div>
         </div>
 
-        <hr />
 
         <div className='columns'>
           <div className='column'>
@@ -116,7 +115,7 @@ export class PackageDetails extends Component {
               >
                 {state => (
                   <div className='message-body message--cta has-text-centered slide-exit message-endorse--question'>
-                    <p className='message-body--text has-text-grey'>
+                    <p className='message-body--text'>
                       Would you endorse this package?
                     </p>
                     <button
@@ -127,7 +126,7 @@ export class PackageDetails extends Component {
                     </button>
 
                     <button
-                      className='button is-dark is-pill button-right'
+                      className='button is-pill button-right'
                       onClick={(e) => { this.handleVoteClick('no', metadata.name, id) }}
                     >
                         No
