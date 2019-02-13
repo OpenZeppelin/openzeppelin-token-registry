9c680fba7252b83b7f416d5c95e90200ed31465e
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-12 16:08:21 -0800

    Brought researchers index in line with package index (#22)
    * Brought researchers index in line with package index
    
    * Fixed eslint

--- src/components/researchers/ResearchersListItem.jsx
+++ src/components/researchers/ResearchersListItem.jsx
@@ -41,74 +41,68 @@ export const ResearchersListItem = ReactTimeout(class _ResearchersListItem exten
     )
 
     return (
-      <div className='list-item list-item__researcher'>
-        <span
-          className={`${animatingCssClassNames}
-          list-item__cell
-          list-item__cell--id
-          has-text-grey
-          has-text-weight-light
-        `}>
-          <Link
-            to={link}
-            className='no-select title is-size-4 has-text-weight-normal'
-          >
-            #{this.props.index + 1} &nbsp;
-          </Link>
-        </span>
+      <div className='list-item-container'>
+        <div className='list-item list-item__researcher'>
+          <span
+            className={`${animatingCssClassNames}
+            list-item__cell
+            list-item__cell--id
+            has-text-grey
+            has-text-weight-light
+          `}>
+            <Link
+              to={link}
+              className='no-select title is-size-4 has-text-weight-normal'
+            >
+              #{this.props.index + 1} &nbsp;
+            </Link>
+          </span>
 
-        <span className={`
-          ${animatingCssClassNames}
-          list-item__cell
-          list-item__cell--total-vouched
-        `}>
-          <Link
-            to={link}
-            className='no-select'
-          >
-            <h6 className='list-item__cell--total-vouched__title is-size-6 has-text-weight-bold is-monospaced has-text-grey'>
-              TOTAL VOUCHED
-            </h6>
-            <h3 className='list-item__cell--total-vouched__value is-size-3 is-monospaced'>
-              <span className='item--version has-text-black has-text-weight-light'>
-                <ZepTokenLogo width='22' height='22' className='researcher--zep-token-icon' />{displayWeiToEther(this.props.researcher.amount)}
+          <span className={`
+            ${animatingCssClassNames}
+            list-item__cell
+            list-item__cell--title
+            list-item__cell--researcher-address
+          `}>
+            <Link
+              to={link}
+              className='no-select'
+            >
+              <h5 className='title is-size-4 is-monospaced has-text-link'>
+                <EnsName address={this.props.researcher.address} shorten />
+              </h5>
+            </Link>
+          </span>
+
+          <span className={`
+            ${animatingCssClassNames}
+            list-item__cell
+            list-item__cell--view-more
+            has-text-right
+          `}>
+            <Link
+              to={link}
+              className='no-select list-item--view-grid'
+            >
+              <span className='list-item--view-grid'>
+
+                <h6 className='subtitle is-size-7 list-item--subtitle is-monospaced'>
+                VOUCHED
+                </h6>
+
+                <ZepTokenLogo width='20' height='20' className='list-item--zep-token-logo' />
+
+                <h3 className='is-inline-block is-size-3 has-text-weight-light list-item--num-tokens'>
+                  {displayWeiToEther(this.props.researcher.amount)}
+                </h3>
+
+                <span className='has-text-info is-size-6 is-monospaced list-item--view-more-link'>
+                View More&gt;
+                </span>
               </span>
-            </h3>
-          </Link>
-        </span>
-
-        <span className={`
-          ${animatingCssClassNames}
-          list-item__cell
-          list-item__cell--title
-          list-item__cell--researcher-address
-        `}>
-          <Link
-            to={link}
-            className='no-select'
-          >
-            <h5 className='title is-size-5 is-monospaced has-text-link'>
-              <EnsName address={this.props.researcher.address} />
-            </h5>
-          </Link>
-        </span>
-
-        <span className={`
-          ${animatingCssClassNames}
-          list-item__cell
-          list-item__cell--view-more
-          has-text-right
-        `}>
-          <Link
-            to={link}
-            className='no-select list-item--view-grid'
-          >
-            <span className='has-text-info is-size-6 is-monospaced list-item--view-more-link list-item--view-more-link__researcher'>
-              View More &gt;
-            </span>
-          </Link>
-        </span>
-
+            </Link>
+          </span>
+        </div>
       </div>
     )
   }
