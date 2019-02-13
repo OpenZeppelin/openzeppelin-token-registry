337097b460b253435ff19714852adbd3fcde45a1
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-12 17:56:10 -0800

    Added delay to ScrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,13 +1,25 @@
 import { PureComponent } from 'react'
+import { PropTypes } from 'prop-types'
+import ReactTimeout from 'react-timeout'
 
-export const ScrollToTop = class _ScrollToTop extends PureComponent {
+export const ScrollToTop = ReactTimeout(class _ScrollToTop extends PureComponent {
+  static propTypes = {
+    delay: PropTypes.number
+  }
+
+  static defaultProps = {
+    delay: 200
+  }
+
   componentDidMount () {
-    if (window) {
-      window.scrollTo(0, 0)
-    }
+    this.props.setTimeout(() => {
+      if (window) {
+        window.scrollTo(0, 0)
+      }
+    }, this.props.delay)
   }
 
   render () {
     return null
   }
-}
+})
